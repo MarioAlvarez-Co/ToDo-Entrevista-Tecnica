2fc762f81d040a237a445cffd78745264278e8a0
Mario <lushoalvarez45@gmail.com>
2022-05-04 16:36:35 -0500

    Updates

---
+++ precache-manifest.0ada1586c61168dfc1aa005069498a58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f57880bc1b3030144618",
+    "url": "/ToDo-Entrevista-Tecnica/static/css/main.3e5a4402.chunk.css"
+  },
+  {
+    "revision": "f57880bc1b3030144618",
+    "url": "/ToDo-Entrevista-Tecnica/static/js/main.f57880bc.chunk.js"
+  },
+  {
+    "revision": "f3453b1efb2089581fae",
+    "url": "/ToDo-Entrevista-Tecnica/static/js/1.f3453b1e.chunk.js"
+  },
+  {
+    "revision": "0dde14c83deaaa90a2ff",
+    "url": "/ToDo-Entrevista-Tecnica/static/js/runtime~main.0dde14c8.js"
+  },
+  {
+    "revision": "20e644c1523ee66b23616c4db027db2a",
+    "url": "/ToDo-Entrevista-Tecnica/index.html"
+  }
+];
