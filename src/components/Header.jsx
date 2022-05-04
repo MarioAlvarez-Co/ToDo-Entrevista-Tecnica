c7ce4acca50a295f81135217ab0f382a2af5b7dc
Mario <lushoalvarez45@gmail.com>
2022-05-04 17:06:19 -0500

    debugeo 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 function Header(){
     return(
         <nav className='Header'>
-            <Link to="/"> <h1>To-Dog App</h1>   </Link>
+            <Link to="/ToDo-Entrevista-Tecnica"> <h1>To-Dog App</h1>   </Link>
               
             
             <span>User</span>   
